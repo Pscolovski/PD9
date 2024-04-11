95f830b301c3c8b7f83fcdabad262684bf9ced4d
Pscolovski <157747460+Pscolovski@users.noreply.github.com>
2024-04-11 11:25:01 -0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let numberX = 10;
+let numberY = '20';
+
+console.log(numberX == numberY);
+console.log(numberX === numberY);
+console.log(numberX != numberY);
+console.log(numberX !== numberY);
+console.log(numberX > numberY);
+console.log(numberX >= numberY);
+console.log(numberX < numberY);
+console.log(numberX <= numberY);
+console.log((numberX != numberY) ? 'It is true' : 'It is false');
